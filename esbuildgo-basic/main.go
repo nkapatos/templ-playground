7c820cc0f2d372a1334010757d682dfc409ef414
Nikos Kapatos <nikos@kapatos.com>
2025-06-19 09:43:36 +0200

    esbuildgo

---
+++ esbuildgo-basic/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/nkapatos/templ-playground/esbuild-go/templ/views"
+)
+
+func main() {
+	// Route handlers
+	http.Handle("/", templ.Handler(views.Home()))
+	http.Handle("/about", templ.Handler(views.About()))
+	http.Handle("/contact", templ.Handler(views.Contact()))
+	http.Handle("/daisyui", templ.Handler(views.DaisyUI()))
+
+	// Static assets
+	fs := http.FileServer(http.Dir("dist"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	fmt.Println("Listening on :8080")
+	http.ListenAndServe(":8080", nil)
+}
