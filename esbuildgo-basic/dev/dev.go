b8fd2aaf735eb77e69157b8273698aeac869ea0d
Nikos Kapatos <nikos@kapatos.com>
2025-06-19 13:15:58 +0200

    setup watch mode with esbuild go pkg

--- esbuildgo-basic/dev/dev.go
+++ esbuildgo-basic/dev/dev.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"github.com/evanw/esbuild/pkg/api"
+)
 
 func main() {
-	api := esbuild
+	ctx, err := api.Context(api.BuildOptions{
+		EntryPoints: []string{"assets/css/index.css", "assets/ts/main.ts"},
+		Outdir:      "dist",
+		Bundle:      true,
+		LogLevel:    api.LogLevelInfo,
+	})
+
+	if err != nil {
+		panic("oooops")
+	}
+
+	watch_error := ctx.Watch(api.WatchOptions{})
+
+	if watch_error != nil {
+		panic("error while watching")
+	}
+
+	select {}
+
 }
